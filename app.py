ec8f6522689eb5f6698663587d849b0ad2ece2f4
koharu <k.watanabe.sys22@morijyobi.ac.jp>
2023-06-08 12:47:57 +0900

    [update]top-menu.html追記

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template,Blueprint
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('top-menu.html')
 
 
 if __name__ == '__main__':
