e337846f0dbfd553d4737dc6cc6888c77babef5b
koharu <k.watanabe.sys22@morijyobi.ac.jp>
2023-06-09 17:27:11 +0900

    [add]top_menu.js追加とデジタル時計実装

---
+++ static/js/top_menu.js
@@ -0,0 +1,17 @@
+function set2fig(num) {
+   // 桁数が1桁だったら先頭に0を加えて2桁に調整する
+   var ret;
+   if( num < 10 ) { ret = "0" + num; }
+   else { ret = num; }
+   return ret;
+}
+function showClock2() {
+   var nowTime = new Date();
+   var nowHour = set2fig( nowTime.getHours() );
+   var nowMin  = set2fig( nowTime.getMinutes() );
+   var h = nowHour
+   var m = nowMin
+   document.getElementById("now_hour").innerHTML = h;
+   document.getElementById("now_min").innerHTML = m;
+}
+setInterval('showClock2()',1000);
