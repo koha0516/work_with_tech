eb696db4064839de775cb8189a8336fd1bde0468
koharu <k.watanabe.sys22@morijyobi.ac.jp>
2023-06-14 09:35:22 +0900

    [add]管理者の申請承認ページを追加
    apply.html
    checkbox.jsなど

--- admin.py
+++ admin.py
@@ -5,3 +5,7 @@ admin_bp = Blueprint('admin', __name__, url_prefix='/admin')
 @admin_bp.route('login')
 def admin_login():
     return render_template('admin/login.html')
+
+@admin_bp.route('apply')
+def admin_apply():
+    return render_template('admin/apply.html')
