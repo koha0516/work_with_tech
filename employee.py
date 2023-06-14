84a6464ad1bce5a6f3d11b55fb26b91158461953
koharu <k.watanabe.sys22@morijyobi.ac.jp>
2023-06-14 13:50:40 +0900

    [add]従業員メニューを追加

--- employee.py
+++ employee.py
@@ -5,3 +5,7 @@ employee_bp = Blueprint('employee', __name__, url_prefix='/employee')
 @employee_bp.route('login')
 def employee_login():
     return render_template('employee/login.html')
+
+@employee_bp.route('menu')
+def employee_menu():
+    return render_template('employee/menu.html')
