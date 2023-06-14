d3ca140ebacd0bd9c87875a2d107b25f4d470490
koharu <k.watanabe.sys22@morijyobi.ac.jp>
2023-06-14 13:50:54 +0900

    [add]管理者メニューを追加

--- admin.py
+++ admin.py
@@ -9,3 +9,7 @@ def admin_login():
 @admin_bp.route('apply')
 def admin_apply():
     return render_template('admin/apply.html')
+
+@admin_bp.route('menu')
+def admin_menu():
+    return render_template('admin/menu.html')
