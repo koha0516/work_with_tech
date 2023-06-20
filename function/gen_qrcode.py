b5567efd9244ca446903f66e7ab9501251ea521a
koharu <k.watanabe.sys22@morijyobi.ac.jp>
2023-06-20 15:06:38 +0900

    [add]qrコード生成機能と読み取り機能の実装

---
+++ function/gen_qrcode.py
@@ -0,0 +1,15 @@
+﻿import qrcode
+import os
+from PIL import Image
+
+qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+
+qr.add_data('watanabe_koharu4224112')
+
+img = qr.make_image()
+
+# file_path = os.path.expanduser('~/Downloads/QRCode.png')
+file_path = './images/employee_id/name_id.png'
+
+img.save(file_path)
+
